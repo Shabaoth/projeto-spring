35371f5890f3f3ef72a57e1853694627dfe024a8
Shiguemori <shiguemori@hotmail.com>
2021-10-28 22:40:24 -0300

    Add model user

---
+++ src/main/java/br/com/projetospring/models/User.java
@@ -0,0 +1,36 @@
+package br.com.projetospring.models;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@RequiredArgsConstructor
+@ToString
+public class User {
+    @Column(nullable = false)
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String name;
+
+    private String email;
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
